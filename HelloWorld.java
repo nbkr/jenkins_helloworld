ce86c13144105f60c16aa5daa0e9d31129bfc46d
Ben Fleckenstein <bf@b1t.company>
2021-06-28 14:29:14 +0200

    Switching to a servlet

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,12 +1,38 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+/**
+ * Servlet implementation class HelloWorld
+ */
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
 
-public class HelloWorld {
+	/**
+	 * Default constructor.
+	 */
+	public HelloWorld() {
+	}
 
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+	/*
+	 * This method will handle all GET request.
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 
+        PrintWriter pw = response.getWriter();
+		pw.println("Hello World");
+	}
 
+	/*
+	 * This method will handle all POST request
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 
-    }
+	}
 
 }

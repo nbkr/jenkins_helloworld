9dd2e23c146401164770c80acf33ec5b9868169b
Ben Fleckenstein <bf@b1t.company>
2021-07-14 21:04:09 +0200

    Add servlet-api

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,34 +5,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class HelloWorld
- */
 public class HelloWorld extends HttpServlet {
-	private static final long serialVersionUID = 1L;
 
-	/**
-	 * Default constructor.
-	 */
-	public HelloWorld() {
-	}
-
-	/*
-	 * This method will handle all GET request.
-	 */
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 
         PrintWriter pw = response.getWriter();
 		pw.println("Hello World");
-	}
-
-	/*
-	 * This method will handle all POST request
-	 */
-	protected void doPost(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-
 	}
 
 }
